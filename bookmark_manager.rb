61ff030e0b1c44797d489ac7e7c80a79d808b183
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-11-14 12:45:31 +0000

    Adds list of links in view

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -1,6 +1,12 @@
 require 'sinatra/base'
+require './models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
+  end
 
   run if app_file==$0
 end

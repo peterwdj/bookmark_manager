19f902241cc25ba82b587653ef78f874ba074827
Peter Johnstone <peter.wdj@gmail.com>
2017-11-14 12:08:49 +0000

    Created Link model

--- spec/features/bookmark_list_spec.rb
+++ spec/features/bookmark_list_spec.rb
@@ -1,7 +1,7 @@
 feature 'seeing a list of links on the homepage' do
   scenario 'listing links' do
-    Link.create(name: 'Google', url: 'https://google.com', description: 'Search stuff')
-    visit '/'
+    Link.create(title: 'Google', url: 'https://google.com')
+    visit '/links'
     within 'ul#links' do
       expect(page).to have_content 'Google'
     end
